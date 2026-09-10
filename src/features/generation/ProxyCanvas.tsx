d6f154e0d0e3bacd03790bd994efc5911635a2f4
Dalton Mankin <dalton.mankin@gmail.com>
2026-09-09 22:53:09 -0400

    fix: clean up ProxyCanvas drawing logic and remove debug statements

--- src/features/generation/ProxyCanvas.tsx
+++ src/features/generation/ProxyCanvas.tsx
@@ -6,36 +6,30 @@ export default function ProxyCanvas({ cardData }: { cardData: CardData }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const drawCard = (context: CanvasRenderingContext2D, cardData: CardData) => {
-
     if (cardData.illustration) {
       const reader = new FileReader();
       reader.readAsDataURL(cardData.illustration as File);
 
       reader.onloadend = () => {
-        console.log("ProxyCanvas File content:", reader.result);
         const img = new Image();
         img.src = reader.result as string;
         img.onload = () => {
           context.clearRect(0, 0, context.canvas.width, context.canvas.height);
           context.fillStyle = '#000000'
           context.font = "24px serif";
-
+          context.strokeRect(0, 0, context.canvas.width, context.canvas.height);
           context.drawImage(img, 5, 30, 240, 240 * (img.height / img.width));
           context.fillText(cardData.cardName, 5, 25);
           context.fillText(cardData.manaCost, 225, 25);
           context.fillText(cardData.typeLine, 5, 240);
           context.fillText(cardData.textBox, 5, 260);
-          context.fillText(cardData.powerToughness, 200, 350);
+          context.fillText(cardData.powerToughness, 200, 345);
         }
       };
     }
-
-    
   }
 
   useEffect(() => {
-    console.log("ProxyCanvas cardData:", cardData);
-
     const canvas = canvasRef.current;
     if (!canvas) return;
     const context = canvas.getContext('2d');
@@ -48,7 +42,6 @@ export default function ProxyCanvas({ cardData }: { cardData: CardData }) {
     <Fragment>
       <h2>Proxy Canvas</h2>
       <canvas ref={canvasRef} width={250} height={350}></canvas>
-      <pre>{ JSON.stringify(cardData, null, 2) }</pre>
     </Fragment>
   );
 }
