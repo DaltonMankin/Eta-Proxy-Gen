27e1015daf1cc8b4a7367ce2ed661e5205dc1c12
Dalton Mankin <dalton.mankin@gmail.com>
2026-09-11 23:32:10 -0400

    fix: refactor drawing logic in ProxyP5 to improve layout and margin handling

--- src/features/generation/ProxyP5.tsx
+++ src/features/generation/ProxyP5.tsx
@@ -13,17 +13,41 @@ export default function ProxyP5({ cardData }: { cardData: CardData }) {
     }
 
     s.draw = async () => {
+      const MARGIN = 4;
+
       s.background(255);
-      s.textSize(24);
 
       if (cardData.illustrationUrl) {
         const img = await s.loadImage(cardData.illustrationUrl as string);
-        s.image(img, 5, 30, 240, 240 * (img.height / img.width));
+        s.image(img, MARGIN * 2, MARGIN * 2, 250 - (MARGIN * 4), (250 - (MARGIN * 4)) * (img.height / img.width));
       }
-      s.text(cardData.cardName, 5, 25);
-      s.text(cardData.manaCost, 225, 25);
+
+      s.textSize(24);
+      s.textAlign(s.LEFT, s.TOP);
+      let cardNameBounds = s.fontBounds(cardData.cardName, 0, 0);
+      s.fill(255);
+      s.noStroke();
+      s.rect(MARGIN, MARGIN, cardNameBounds.w + MARGIN, cardNameBounds.h + (MARGIN / 2));
+      s.fill(0);
+      s.text(cardData.cardName, MARGIN + MARGIN - (MARGIN / 2), MARGIN + (MARGIN / 2));
+
+      s.textSize(24);
+      s.textAlign(s.LEFT, s.TOP);
+      let manaCostBounds = s.fontBounds(cardData.manaCost, 0, 0);
+      s.fill(255);
+      s.noStroke();
+      s.rect(s.width - (manaCostBounds.w + MARGIN + MARGIN), MARGIN, manaCostBounds.w + MARGIN, manaCostBounds.h + (MARGIN / 2));
+      s.fill(0);
+      s.text(cardData.manaCost, s.width - (manaCostBounds.w + MARGIN + MARGIN - (MARGIN / 2)), MARGIN + (MARGIN / 2));
+
+      s.textSize(24);
+      s.textAlign(s.LEFT, s.TOP);
       s.text(cardData.typeLine, 5, 240);
+
+      s.textSize(24);
       s.text(cardData.textBox, 5, 260);
+
+      s.textSize(24);
       s.text(cardData.powerToughness, 200, 345);
     }
   };
